4e969b31d8f3dca2e72e93228d3f2024c75e5691
Elena <angry-elena@yandex.ry>
2024-09-14 19:14:13 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,12 @@
+
+URL_SERVICE = "https://f1f27d64-130f-4de3-824c-cba222e3900c.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
